23bd0240c73ddf711ecdd074f9e7cf22f093ffa9
returntears <returntears@outlook.com>
2020-08-23 19:26:06 +0800

    SpringBoot 缓存实体类实现Serializable序列化接口

--- allst-springboot-advs/src/main/java/com/allst/springboot/bean/Department.java
+++ allst-springboot-advs/src/main/java/com/allst/springboot/bean/Department.java
@@ -2,12 +2,15 @@ package com.allst.springboot.bean;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
+ * 使用缓存的对象实体类需要实现Serializable序列化接口
  * @author YiYa
  * @since 2020-08-23 上午 01:07
  */
 @Data
-public class Department {
+public class Department implements Serializable {
     private Integer id;
     private String departmentName;
     private Integer employeeNums;
---
+++ allst-springboot-advs/src/main/java/com/allst/springboot/config/RedisConfig.java
@@ -0,0 +1,56 @@
+package com.allst.springboot.config;
+
+import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * 自定义Redis配置类
+ *
+ * @author YiYa
+ * @since 2020/8/6-10:20
+ */
+@ConditionalOnClass(RedisOperations.class)
+@EnableConfigurationProperties(RedisProperties.class)
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    @ConditionalOnMissingBean(name = "redisTemplate")
+    public RedisTemplate<String, Object> redisTemplate(
+            RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        // 序列化配置
+        // 使用fastjson序列化
+        FastJsonRedisSerializer fastJsonRedisSerializer = new FastJsonRedisSerializer<>(Object.class);
+        // value值的序列化采用fastJsonRedisSerializer
+        template.setValueSerializer(fastJsonRedisSerializer);
+        template.setHashValueSerializer(fastJsonRedisSerializer);
+        // key的序列化采用StringRedisSerializer
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(StringRedisTemplate.class)
+    public StringRedisTemplate stringRedisTemplate(
+            RedisConnectionFactory redisConnectionFactory) {
+        StringRedisTemplate template = new StringRedisTemplate();
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+
+}
