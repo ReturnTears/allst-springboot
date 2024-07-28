59fa4ecdab6f2fde09098cf73ac986bbb658ee5d
returntears <returntears@outlook.com>
2024-07-28 16:52:36 +0800

    准备Start Docker之旅

---
+++ allst-springboot-docker/src/main/java/com/allst/boot/DockerApplication.java
@@ -0,0 +1,11 @@
+package com.allst.boot;
+
+/**
+ * @author Hutu
+ * @since 2024-07-28 下午 04:50
+ */
+public class DockerApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
