6a73e665dab13a3c4416b57286ab8fb7b50b2bd1
returntears <returntears@outlook.com>
2024-07-28 22:56:54 +0800

    准备Start K8s之旅

---
+++ allst-springboot-k8s/src/main/java/com/allst/boot/K8sApplication.java
@@ -0,0 +1,11 @@
+package com.allst.boot;
+
+/**
+ * @author Hutu
+ * @since 2024-07-28 下午 10:52
+ */
+public class K8sApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
