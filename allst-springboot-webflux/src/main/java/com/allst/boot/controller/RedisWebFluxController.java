aadf26a25af44ce93727b9535a4e6276a241e9d9
returntears <returntears@outlook.com>
2024-08-17 22:25:34 +0800

    SpringBoot+WebFlux+Redis实现缓存

--- allst-springboot-webflux/src/main/java/com/allst/boot/controller/RedisWebFluxController.java
+++ allst-springboot-webflux/src/main/java/com/allst/boot/controller/RedisWebFluxController.java
@@ -1,7 +1,13 @@
 package com.allst.boot.controller;
 
+import com.allst.boot.domain.Book;
+import com.allst.boot.repository.BookRepository;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.core.ReactiveValueOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
@@ -12,6 +18,13 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequestMapping(value = "/redis")
 public class RedisWebFluxController {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @Autowired
     private ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
 
@@ -22,4 +35,16 @@ public class RedisWebFluxController {
         return mono;
     }
 
+    @GetMapping(value = "/set/cache/{key}")
+    public Mono<String> setCacheBy(@PathVariable("key") String key) {
+        Book book = bookRepository.findBookByName(key);
+        stringRedisTemplate.opsForValue().set(key, new Gson().toJson(book));
+        return Mono.just("Cache Success !");
+    }
+
+    @GetMapping(value = "/get/cache/{key}")
+    public Mono<String> getCacheBy(@PathVariable("key") String key) {
+        ReactiveValueOperations<String, String> value = reactiveRedisTemplate.opsForValue();
+        return value.get(key);
+    }
 }
