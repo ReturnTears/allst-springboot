1e7f0a8c356c9e88f6114c0c31e5158af9ce15c1
returntears <returntears@outlook.com>
2024-08-23 22:32:13 +0800

    @RequestMapping

--- allst-springboot-demo/src/main/java/com/allst/springboot/controller/DemoController.java
+++ allst-springboot-demo/src/main/java/com/allst/springboot/controller/DemoController.java
@@ -1,8 +1,6 @@
 package com.allst.springboot.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Hutu
@@ -14,5 +12,50 @@ public class DemoController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello Demo~";
+    }
+
+    /**
+     * 多个映射
+     */
+    @GetMapping({"/multi_1", "/multi_2", "/multi_*", "/more_uri"})
+    public String multiMapping() {
+        return "Hello Multi Mapping~";
+    }
+
+    /**
+     * 处理地址中的参数
+     */
+    @GetMapping("/path/{id}")
+    public String pathVariable(@PathVariable("id") Integer paramId) {
+        System.out.println("id = " + paramId);
+        return "Hello Path Variable 1~";
+    }
+
+    @GetMapping("/get/id")
+    public String getId(@RequestParam("id") Integer paramId) {
+        System.out.println("id = " + paramId);
+        return "Hello Path Variable 2~";
+    }
+
+    @GetMapping("/getVal/id")
+    public String getValueId(@RequestParam Integer id) {
+        System.out.println("id = " + id);
+        return "Hello Path Variable 3~";
+    }
+
+    @GetMapping("/name")
+    public String getName(@RequestParam(value = "username", required = false) String name) {
+        System.out.println("name = " + name);
+        return "name ~ " + name;
+    }
+
+    /**
+     * 实现动态URL
+     */
+    @RequestMapping(value = "/fetch/{id:[a-z]+}/{name}", method = RequestMethod.GET)
+    public String getDynamicUrl(@PathVariable(value = "name") String name, @PathVariable(value = "id") String id) {
+        System.out.println("name = " + name);
+        System.out.println("id = " + id);
+        return "DynamicUrl ~ " + name + " , " + id;
     }
 }
