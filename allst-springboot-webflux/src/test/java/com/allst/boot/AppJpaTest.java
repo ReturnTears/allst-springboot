a5550e683616e246aaa6926d0f4b052088c79c88
returntears <returntears@outlook.com>
2024-09-29 23:21:47 +0800

    SpringBoot测试JdbcTemplate

--- allst-springboot-webflux/src/test/java/com/allst/boot/AppJpaTest.java
+++ allst-springboot-webflux/src/test/java/com/allst/boot/AppJpaTest.java
@@ -1,5 +1,6 @@
 package com.allst.boot;
 
+import com.allst.boot.domain.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,7 +8,9 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.List;
 
 @SpringBootTest
 public class AppJpaTest {
@@ -33,5 +36,53 @@ public class AppJpaTest {
             ps.setInt(2, 490);
             return ps.executeUpdate();
         });
+    }
+
+    @Test
+    public void TestPreparedStatementSetter() {
+        String sql = "insert into book (name, pageCount) values (?, ?)";
+        jdbcTemplate.update(sql, (PreparedStatement ps) -> {
+           ps.setString(1, "Vue实战");
+           ps.setInt(2, 520);
+        });
+    }
+
+    @Test
+    public void testResultSetExtractor() {
+        String sql = "select * from book where id = ?";
+        Book result = jdbcTemplate.query(sql, (ResultSet rs) -> {
+            rs.next();
+            Book book = new Book();
+            book.setId(rs.getLong("id"));
+            book.setName(rs.getString("name"));
+            book.setPageCount(rs.getString("pageCount"));
+            return book;
+        }, 13);
+        System.out.println(result);
+    }
+
+    @Test
+    public void testRowCallbackHandler() {
+        String sql = "select * from book where id = ?";
+        Book book = new Book();
+        jdbcTemplate.query(sql, (ResultSet rs) -> {
+            book.setId(rs.getLong("id"));
+            book.setName(rs.getString("name"));
+            book.setPageCount(rs.getString("pageCount"));
+        }, 15);
+        System.out.println(book);
+    }
+
+    @Test
+    public void testRowMapper() {
+        String sql = "select * from book";
+        List<Book> bookList = jdbcTemplate.query(sql, (rs, rowNum) -> {
+            Book book = new Book();
+            book.setId(rs.getLong("id"));
+            book.setName(rs.getString("name"));
+            book.setPageCount(rs.getString("pageCount"));
+            return book;
+        });
+        bookList.forEach(System.out::println);
     }
 }
